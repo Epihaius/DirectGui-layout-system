180c7cef4b58f19af41518479442bf19959098e8
Epihaius <Epihaius@users.noreply.github.com>
2020-10-08 19:12:46 +0200

    Version 1.0.0

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,8 +1,9 @@
 # Author: Epihaius
 # Date: 2019-09-23
+# Last revision: 2020-10-08
 #
 # This package contains code to create an automatic GUI layout system.
 
 from .gui import GUI
-from .sizer import Sizer, GridSizer
+from .sizer import Sizer
 from .widget import Widget, ScrolledListWidget, ScrolledFrameWidget
